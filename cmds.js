48639078284b98d4985e270767963230742b37cd
Manuel <manuel.rdelacoba@alumnos.upm.es>
2018-03-04 19:02:18 +0100

    quiz

---
+++ cmds.js
@@ -0,0 +1,153 @@
+const model = require("./model");
+const { colorize, log, biglog, errorlog } = require("./out");
+
+exports.helpCmd = rl => {
+  log("Commandos: ");
+  log("h|help - Muestra esta ayuda.");
+  log("list - Listar los quizzes existentes.");
+  log("show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
+  log("add - Añadir un nuevo quiz interactivamente.");
+  log("delete <id> - Borrar el quiz indicado.");
+  log("edit <id> - Editar el quiz indicado.");
+  log("test <id> - Probar el quiz indicado.");
+  log("p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+  log("credits - Créditos.");
+  log("q|quit - Salir del programa.");
+  rl.prompt();
+}
+
+exports.listCmd = rl => {
+  model.getAll().forEach((quiz, id) => {
+    log(`[${colorize(id, "magenta")}]: ${quiz.question}`);
+  });
+  rl.prompt();
+}
+
+exports.showCmd = (rl, id) => {
+  if (typeof id === "undefined") {
+    errorlog(`Falta el parámetro id.`);
+  } else {
+    try {
+      const quiz = model.getByIndex(id);
+      log(`[${colorize(id, "magenta")}]: ${quiz.question} ${colorize("=>", "magenta")} ${quiz.answer}`);
+    } catch (error) {
+      errorlog(error.message);
+    }
+  }
+  rl.prompt();
+}
+
+exports.addCmd = (rl, id) => {
+  rl.question(colorize("Introduzca una pregunta: ", "red"), question => {
+    rl.question(colorize("Introduzca la respuesta ", "red"), answer => {
+      model.add(question, answer);
+      log(`${colorize("Se ha añadido", "magenta")}: ${question} ${colorize("=>", "magenta")} ${answer}`);
+      rl.prompt();
+    });
+  });
+}
+
+exports.deleteCmd = (rl, id) => {
+  if (typeof id === "undefined") {
+    errorlog(`Falta el parámetro id.`);
+  } else {
+    try {
+      model.deleteByIndex(id);
+    } catch (error) {
+      errorlog(error.message);
+    }
+  }
+  rl.prompt();
+}
+
+exports.editCmd = (rl, id) => {
+  if (typeof id === "undefined") {
+    errorlog(`Falta el parámetro id.`);
+    rl.prompt();
+  } else {
+    try {
+      const quiz = model.getByIndex(id);
+      process.stdout.isTTY && setTimeout(() => { rl.write(quiz.question) }, 0);
+      rl.question(colorize("Introduzca una pregunta: ", "red"), question => {
+        process.stdout.isTTY && setTimeout(() => { rl.write(quiz.answer) }, 0);
+        rl.question(colorize("Introduzca la respuesta ", "red"), answer => {
+          model.update(id, question, answer);
+          log(`Se ha cambiado el quiz ${colorize(id, "magenta")} por: ${question} ${colorize("=>", "magenta")} ${answer}`);
+          rl.prompt();
+        });
+      });
+    } catch (error) {
+      errorlog(error.message);
+      rl.prompt();
+    }
+  }
+}
+
+exports.testCmd = (rl, id) => {
+  if (typeof id === "undefined") {
+    errorlog(`Falta el parámetro id.`);
+    rl.prompt();
+  } else {
+    try {
+      const quiz = model.getByIndex(id);
+      rl.question(colorize(quiz.question, "red"), ans => {
+        if (ans.toLowerCase().trim() == quiz.answer.toLowerCase().trim()) {
+          biglog("Correcto", "green");
+          rl.prompt();
+        } else {
+          biglog("Incorrecto", "red");
+          rl.prompt();
+        }
+      });
+    } catch (error) {
+      errorlog(error.message);
+      rl.prompt();
+    }
+  }
+}
+
+exports.playCmd = rl => {
+  let score = 0;
+  let size = model.count();
+  let toBeResolved = new Array(size);
+  for (var x = 0; x < size; x++) {
+    toBeResolved[x] = x;
+  }
+  const jugar = () => {
+    if (toBeResolved.length <= 0) {
+      biglog("¡Enhorabuena!", "green");
+      log(`Has ganado. Preguntas acertadas: ${colorize(score, "yellow")}`, "green");
+      rl.prompt();
+    } else {
+      let azar = Math.floor(Math.random() * toBeResolved.length);
+      let id = toBeResolved[azar];
+      toBeResolved.splice((toBeResolved.length - 1), 1);
+      let quiz = model.getByIndex(id);
+      model.deleteByIndex(id);
+      rl.question(colorize(quiz.question, "red"), ans => {
+        if (ans.toLowerCase().trim() == quiz.answer.toLowerCase().trim()) {
+          score++;
+          biglog("Correcto", "green");
+          log(`Preguntas acertadas: ${colorize(score, "yellow")}`, "green");
+          jugar();
+        } else {
+          biglog("Incorrecto", "red");
+          log(`Has perdido. Preguntas acertadas: ${colorize(score, "yellow")}`, "green");
+          rl.prompt();
+        }
+      });
+    }
+  }
+  jugar();
+}
+
+exports.creditsCmd = rl => {
+  log("Autores de la práctica: ");
+  log("Manuel Rodríguez de la Coba");
+  rl.prompt();
+}
+
+exports.quitCmd = rl => {
+  rl.close();
+  rl.prompt();
+}
---
+++ main.js
@@ -0,0 +1,70 @@
+const readline = require("readline");
+const cmds = require("./cmds");
+const { colorize, log, biglog, errorlog } = require("./out");
+
+biglog("CORE Quiz", "green");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: colorize("quiz > ", "blue"),
+  completer: (line) => {
+    const completions = "h help list show add delete edit test p play credits q quit".split(' ');
+    const hits = completions.filter((c) => c.startsWith(line));
+    return [hits.length ? hits : completions, line];
+  }
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+
+  let args = line.split(" ");
+  let cmd = args[0].toLowerCase().trim();
+
+  switch (cmd) {
+    case "":
+      break;
+    case "h":
+    case "help":
+      cmds.helpCmd(rl);
+      break;
+    case "list":
+      cmds.listCmd(rl);
+      break;
+    case "show":
+      cmds.showCmd(rl, args[1]);
+      break;
+    case "add":
+      cmds.addCmd(rl, args[1]);
+      break;
+    case "delete":
+      cmds.deleteCmd(rl, args[1]);
+      break;
+    case "edit":
+      cmds.editCmd(rl, args[1]);
+      break;
+    case "test":
+      cmds.testCmd(rl, args[1]);
+      break;
+    case "p":
+    case "play":
+      cmds.playCmd(rl);
+      break;
+    case "credits":
+      cmds.creditsCmd(rl);
+      break;
+    case "q":
+    case "quit":
+      cmds.quitCmd(rl);
+      break;
+    default:
+      log(`Comando desconocido: "${colorize(cmd, "red")}"`);
+      log(`Use ${colorize("help", "green")} para ver todos los comandos disponibles.`)
+      break;
+  }
+
+}).on('close', () => {
+  biglog("Sayonara baby", "red");
+  process.exit(0);
+});
